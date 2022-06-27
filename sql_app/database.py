2ae200f270ff65bfba9cb741d06d2158e557be76
hardikothiya <hardikothiya@gmail.com>
2022-06-27 18:23:02 +0530

    heroku deployment init

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import mysql.connector
 
-
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:hardik991322@localhost:3306/ecom_backend"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:hardik991322@localhost:3306/ecom_backend"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://THybKxI0bq:9XPsxcR7g5@remotemysql.com:3306/THybKxI0bq"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True, connect_args={'use_unicode': "utf-8"}
@@ -14,13 +14,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# cxnn = mysql.connector.connect(host='localhost', database='ecom_backend', user='root', password='hardik991322')
+cxnn = mysql.connector.connect(host='remotemysql.com', database='THybKxI0bq', user='THybKxI0bq', password='9XPsxcR7g5')
 
-cxnn = mysql.connector.connect(host='localhost', database='ecom_backend', user='root', password='hardik991322')
 cursor = cxnn.cursor()
 
-
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://sql6499676:kQCr914jWd@sql6.freemysqlhosting.net:3306/sql6499676"
-#
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, echo=True, connect_args={'use_unicode': "utf-8"}
 # )
